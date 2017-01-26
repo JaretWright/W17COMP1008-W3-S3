26dcfac0ec584d1af0be8f47ea66ee6b5224d1fa
jwright <jwright@10.250.20.174>
2017-01-26 13:49:12 -0500

    Final commit for the day

--- src/w17jan25/W17Jan25.java
+++ src/w17jan25/W17Jan25.java
@@ -23,7 +23,19 @@ public class W17Jan25 {
        emp1.recordHoursWorked(3);
        emp1.recordHoursWorked(8);
        
-       System.out.printf("%s%n",  emp1.getPayCheck());
+      
+       
+        CommissionEmployee emp2 = new CommissionEmployee("Anita","Bevie",12345,
+                                                LocalDate.of(1972, Month.MAY, 10), 5);
+       
+        System.out.printf("The second employee is %s%n", emp2.toString());
+        emp2.recordSales(3234.33);
+        emp2.recordSales(983.33);
+        
+        System.out.printf("%s The employee class is %s%n",  emp1.getPayCheck(),emp1.getClass());
+        System.out.printf("%s The employee class is %s%n",  emp2.getPayCheck(),emp2.getClass());
+       
+       
     }
     
 }
